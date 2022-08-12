8c3ce3ce0a70d89a6ca3125bcde59b265cce67f1
SUNITA DAS MITRA <110525289+SUNITADAS078@users.noreply.github.com>
2022-08-12 20:12:42 +0530

    Add files via upload

---
+++ cal.java.java
@@ -0,0 +1,17 @@
+//Write a Java program to print the subtract,multiply,divide of two numbers.
+
+class Calculate{
+public static void main(String[] args){
+system.out.println("Enter two numbers :");
+int a=24;
+int b=6;
+System.out.println(" your two no is =" +a +" " +b);
+int sub=a-b;
+int mul=a*b;
+int div=a/b;
+System.out.println("your subtraction is =" +sub);
+System.out.println("your multiplication is =" +mul);
+System.out.println("your divide is =" +div);
+   }
+}
+
