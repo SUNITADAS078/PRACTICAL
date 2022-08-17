c344a56db9dc297b6336cbaee29ba5159ba35444
SUNITA DAS MITRA <110525289+SUNITADAS078@users.noreply.github.com>
2022-08-17 13:34:43 +0530

    Add files via upload

---
+++ Arithmatic1.java
@@ -0,0 +1,18 @@
+package operator2;
+
+public class Arithmatic1 {
+
+	public static void main(String[] args) {
+		//initialize a & b
+		        int a = 10;
+		        int b = 20;
+		        System.out.println(a+b);  // addition
+		        System.out.println(a-b);  // subtraction
+		        System.out.println(a*b);  // multiplication
+		        System.out.println(a/b);  //divison
+		        System.out.println(a%b);  // mod
+		        System.out.println(10*5/3+2-1*6/3);
+
+	}
+
+}
